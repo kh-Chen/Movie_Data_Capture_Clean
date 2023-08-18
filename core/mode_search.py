e999c0483b150bc5a7100b55baae5da8dde12605
khchen <18241224605@163.com>
2023-08-18 14:37:50 +0800

    search mode

--- core/mode_search.py
+++ core/mode_search.py
@@ -9,21 +9,21 @@ def run(numbers:str):
     number_arr = numbers.split(",")
     for number in number_arr:
         json_data = scraper.get_base_data_by_number(number)
-        logger.debug(f"json data for number [{number}]: ")
+        logger.info(f"json data for number [{number}]: ")
         try:
-            logger.debug("-------- INFO -------")
+            logger.info("-------- INFO -------")
             for i, v in json_data.items():
-                if i == 'outline':
-                    logger.debug(f'{"%-19s" % i} : {len(v)} characters')
-                    continue
-                if i == 'actor_photo' or i == 'year':
-                    continue
-                if i == 'extrafanart':
-                    logger.debug(f'{"%-19s" % i} : {len(v)} links')
-                    continue
-                logger.debug(f'{i:<{cn_space(i, 19)}} : {v}')
+                # if i == 'outline':
+                #     logger.info(f'{"%-19s" % i} : {len(v)} characters')
+                #     continue
+                # if i == 'actor_photo' or i == 'year':
+                #     continue
+                # if i == 'extrafanart':
+                #     logger.info(f'{"%-19s" % i} : {len(v)} links')
+                #     continue
+                logger.info(f'{i:<{cn_space(i, 19)}} : {v}')
 
-            logger.debug("-------- INFO -------")
+            logger.info("-------- INFO -------")
         except:
             pass
         #TODO 自定义睡眠时间
