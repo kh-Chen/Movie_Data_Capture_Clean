9e6e20f57a5bc7e5909d0e35a34aacafca0d8c6a
khchen <18241224605@163.com>
2023-12-01 11:18:44 +0800

    update printdata

--- core/mode_search.py
+++ core/mode_search.py
@@ -10,19 +10,22 @@ def run(numbers:str):
     for number in number_arr:
         json_data = scraper.get_base_data_by_number(number)
         logger.info(f"json data for number [{number}]: ")
-        try:
-            logger.info("-------- INFO -------")
-            for i, v in json_data.items():
-                if i == 'magnets' and len(v) > 0:
-                    for d in v:
-                        logger.info(f'{i:<{cn_space(i, 14)}}: {d["link"]:<{cn_space(d["link"], 100)}}{d["meta"]:<{cn_space(d["meta"], 15)}} {d["tags"]}')
-                else:
-                    logger.info(f'{i:<{cn_space(i, 14)}}: {v}')
-
-            logger.info("-------- INFO -------")
-        except:
-            pass
+        print_data(json_data)
         interval = config.getIntValue("common.interval")
         if interval != 0:
             logger.info(f"Continue in {interval} seconds")
             time.sleep(interval)
+
+def print_data(json_data):
+    try:
+        logger.info("-------- INFO -------")
+        for i, v in json_data.items():
+            if i == 'magnets' and len(v) > 0:
+                for d in v:
+                    logger.info(f'{i:<{cn_space(i, 14)}}: {d["link"]:<{cn_space(d["link"], 100)}}{d["meta"]:<{cn_space(d["meta"], 15)}} {d["tags"]}')
+            else:
+                logger.info(f'{i:<{cn_space(i, 14)}}: {v}')
+
+        logger.info("-------- INFO -------")
+    except:
+        logger.error("print_data error.")
