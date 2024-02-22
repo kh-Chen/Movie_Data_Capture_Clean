e1036dcc2ec0de2e4baca967e01599e0681d4875
khchen <18241224605@163.com>
2024-02-22 15:26:29 +0800

    番号提取器优化

--- utils/number_parser.py
+++ utils/number_parser.py
@@ -21,8 +21,6 @@ G_TAKE_NUM_RULES = {
     'xxx-av': lambda x: ''.join(['xxx-av-', re.findall(r'xxx-av[^\d]*(\d{3,5})[^\d]*', x, re.I)[0]]),
     'heydouga': lambda x: 'heydouga-' + '-'.join(re.findall(r'(\d{4})[\-_](\d{3,4})[^\d]*', x, re.I)[0]),
     'heyzo': lambda x: 'HEYZO-' + re.findall(r'heyzo[^\d]*(\d{4})', x, re.I)[0],
-    'mdbk': lambda x: str(re.search(r'mdbk(-|_)(\d{4})', x, re.I).group()),
-    'mdtm': lambda x: str(re.search(r'mdtm(-|_)(\d{4})', x, re.I).group()),
     'caribpr': lambda x: str(re.search(r'\d{6}(-|_)\d{3}', x, re.I).group()).replace('_', '-'),
     'fc2': lambda x: "FC2-" + str(re.search(r'(fc2)(-|_){0,1}(ppv){0,1}(-|_){0,1}(\d{7})(?=\D)', x, re.I).group(5)),
 }
@@ -34,13 +32,14 @@ def get_number(file_path: str) -> str:
     filename = os.path.basename(file_path)
     filename = G_SPAT.sub("", filename)
     try:
-        try:
-            for k, v in G_TAKE_NUM_RULES.items():
+        for k, v in G_TAKE_NUM_RULES.items():
+            try:
                 if re.search(k, filename, re.I):
                     return v(filename)
-        except Exception as e:
-            logger.error(f"get_number with G_TAKE_NUM_RULES from [{filename}] error. [{e}]")
-            # print(f"get_number with G_TAKE_NUM_RULES from [{filename}] error. [{e}]")
+            except Exception as e:
+                logger.error(f"get_number with G_TAKE_NUM_RULES[{k}] from [{filename}] error. [{e}]")
+                # print(f"get_number with G_TAKE_NUM_RULES[{k}] from [{filename}] error. [{e}]")
+        
 
         result = re.search(r'([a-zA-Z]{2,6})(-|_{1,})(\d{2,5})',filename)
         if result is None:
@@ -93,7 +92,7 @@ def get_number(file_path: str) -> str:
 #     return bool(G_cache_uncensored_conf.check(number))
 
 
-if __name__ == "__main__":
+def test():
     #     import doctest
     #     doctest.testmod(raise_on_error=True)
     test_use_cases = (
